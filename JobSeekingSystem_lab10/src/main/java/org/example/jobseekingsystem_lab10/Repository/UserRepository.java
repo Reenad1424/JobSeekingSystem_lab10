bcc6bb42fcece35bdea124e4d6cb6d7f85675e15
Reenad Almadhi <ren3011h@gmail.com>
2026-05-12 14:56:43 +0300

    UserRepository.java

--- JobSeekingSystem_lab10/src/main/java/org/example/jobseekingsystem_lab10/Repository/UserRepository.java
+++ JobSeekingSystem_lab10/src/main/java/org/example/jobseekingsystem_lab10/Repository/UserRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
 
-    @Query("select u from User u where u.id=?1 ")
     User findUserById(Integer id);
 
 
