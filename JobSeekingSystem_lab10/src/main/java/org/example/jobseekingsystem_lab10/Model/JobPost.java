23cbd7d52e15e78e04e968089a3b6112c5f8da07
Reenad Almadhi <ren3011h@gmail.com>
2026-05-12 19:17:23 +0300

    JobPost.java

--- JobSeekingSystem_lab10/src/main/java/org/example/jobseekingsystem_lab10/Model/JobPost.java
+++ JobSeekingSystem_lab10/src/main/java/org/example/jobseekingsystem_lab10/Model/JobPost.java
@@ -1,28 +1,42 @@
 package org.example.jobseekingsystem_lab10.Model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.validation.constraints.NotNull; 
 
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class JobApplication {
+public class JobPost {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotNull(message = "User ID cannot be null")
-    @Column(columnDefinition = "int not null")
-    private Integer userId;
+    @NotEmpty(message = "Title cannot be null")
+    @Size(min = 5, message = "The title must be more than 4 letters")
+    @Column(columnDefinition = "varchar(50) not null")
+    private String title;
 
-    @NotNull(message = "Job Post ID cannot be null")
+    @NotEmpty(message = "Description cannot be null")
+    @Column(columnDefinition = "text not null")
+    private String description;
+
+    @NotEmpty(message = "Location cannot be null")
+    @Column(columnDefinition = "varchar(100) not null")
+    private String location;
+
+    @NotNull(message = "Salary cannot be null")
+    @PositiveOrZero(message = "The salary must be a non-negative number")
     @Column(columnDefinition = "int not null")
-    private Integer jobPostId;
+    private Integer salary;
+
+    @Column(columnDefinition = "date", updatable = false)
+    private LocalDate postingDate = LocalDate.now();
+
 }
-
