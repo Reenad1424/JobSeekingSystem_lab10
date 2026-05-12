7c5f6b22403328f8ca1ceb1ba31eac16be5b5254
Reenad Almadhi <ren3011h@gmail.com>
2026-05-12 19:15:51 +0300

    JobApplication.java

--- JobSeekingSystem_lab10/src/main/java/org/example/jobseekingsystem_lab10/Model/JobApplication.java
+++ JobSeekingSystem_lab10/src/main/java/org/example/jobseekingsystem_lab10/Model/JobApplication.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;  
+
 
 @Data
 @AllArgsConstructor
@@ -15,9 +17,11 @@ public class JobApplication {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotNull(message = "User ID cannot be null")
     @Column(columnDefinition = "int not null")
     private Integer userId;
 
+    @NotNull(message = "Job Post ID cannot be null")
     @Column(columnDefinition = "int not null")
     private Integer jobPostId;
 }
